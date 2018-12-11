79398c61ad2c07cd6f190820c43e10e4b5d922d8
Peter Winslow <phwinslow23@gmail.com>
2018-12-10 22:38:05 -0500

    Updated backend for game display

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ var express = require('express');
 var mysql = require('mysql');
 var bcrypt = require('bcryptjs');
 var fs = require('fs');
-var bodyParser = require("body-parser");
-var nbaFile = require('./NBA');
-var gamesFile = require('./Games');
 
 var app = express();
 app.use(express.static('.'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-var games = new gamesFile.Games()
-var nba = new nbaFile.NBA();
+/* Emitter 
+var t = new topics.Topics();
+app.get('/topics', function(req, resp){
+	t.once('Finished', function (msg) {
+		resp.send(msg);
+	})
+	t.getTopics()
+});
+*/
 
 // Connect to database
 var con = mysql.createConnection({
@@ -31,118 +33,46 @@ con.connect(function (err) {
 	}
 });
 
-// Default page
-app.get('/', function (req, resp) { resp.send(fs.readFileSync('SportWatcher.html', 'utf8')); })
-
-// Return team data in database
+// Single database endpoints
 app.get('/teams', function (req, resp) {
 	var queryStr = 'Select * from team;'
 	con.query(queryStr, function (err, rows, fields) {
 		if (err)
 			console.log('Error during query processing: ' + err);
 		else
+			console.log(rows)
 			resp.send(rows);
 	});
 })
 
-// Create an account
-app.post('/signup', function (req, resp) {
-	var user = req.body.user;
-	var password = req.body.password;
-	var team = req.body.team;
+// Single database endpoints
+app.get('/signup', function (req, resp) {
+	var user = req.query.user;
+	var password = req.query.password;
 	var queryStr = 'Select * from user;'
 	con.query(queryStr, function (err, rows, fields) {
 		if (err)
 			console.log('Error during query processing: ' + err);
-		else {
+		else
 			rows = rows.filter(u => u.id === user)
 			if (rows.length === 0) {
-				// Todo: Protect from injection later
-				bcrypt.hash(password, 10, function (err, hash) {
+				// Protect from injection later
+				bcrypt.hash(password, 10, function(err, hash) {
 					var insertStr = "Insert into user (id, pass) VALUES ('" + user + "', '" + hash + "');"
 					con.query(insertStr, function (err) {
-						if (err) {
-							console.log('Error during user insert: ' + err)
-							resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));
-						}
-						else {
-							var queryStr = 'Select id from team WHERE team="' + team + '";'
-							con.query(queryStr, function (err, rows, fields) {
-								if (err) {
-									console.log('Error during team query processing: ' + err);
-									resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));
-								} else {
-									if (rows.length > 0) {
-										var teamID = rows[0].id;
-										console.log(teamID)
-										var insertTeamStr = "Insert into user_team (user_id, team_id) VALUES ('" + user + "', '" + teamID + "');"
-										con.query(insertTeamStr, function (err) {
-											if (err) {
-												console.log('Error during user_team insert: ' + err)
-												resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));
-											}
-											else {
-												games.once('Finished', function (msg) {
-													resp.send(msg);
-												})
-												games.displayGames();
-											}
-										});
-									} else {
-										console.log("Improper team selected");
-										resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));
-									}
-								}
-							});
-						}
+						if (err) {console.log('Error during user insert: ' + err)}
+						else {resp.send(true);}
 					});
 				});
-			} else { resp.send(fs.readFileSync('SportWatcher.html', 'utf8')); }
-		}
-	});
-})
-
-// Validate username password combination
-app.post('/login', function (req, resp) {
-	var user = req.body.user;
-	var password = req.body.password;
-	var queryStr = 'Select * from user;'
-	con.query(queryStr, function (err, rows, fields) {
-		if (err)
-			console.log('Error during query processing: ' + err);
-		else {
-			row = rows.find(u => u.id === user)
-			if (rows.length > 0) {
-				bcrypt.compare(password, row.pass, function (err, result) {
-					if (result){
-						games.once('Finished', function (msg) {
-							resp.send(msg);
-						})
-						games.displayGames();
-					} else {resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));}
-				});
-			} else {resp.send(fs.readFileSync('SportWatcher.html', 'utf8'));}
-		}
+			} else (resp.send(false)) 
+			
 	});
 })
 
 
-// Returns game by id
-app.get('/api/game', function (req, resp) {
-	var id = req.query.id;
-	nba.once('Finished', function (msg) {
-		resp.send(msg);
-	})
-	nba.getGameByID(id);
-})
+// Checking password bcrypt.compare(password, hash, function(err, result) {});
 
-// Returns all games for today
-app.get('/api/games', function (req, resp) {
-	nba.once('Finished', function (msg) {
-		resp.send(msg);
-	})
-	nba.getGames();
-})
+
 
 // Server listener
 app.listen(8080, function () {
